a39f3c15b98d5233a2c1030fb1ba1744fb70fcbe
Vishnu Mohan <65121365+Vishnu-vm@users.noreply.github.com>
2020-08-05 18:29:08 +0530

    Add files via upload

---
+++ Recommendation Engine.R
@@ -0,0 +1,41 @@
+#Installing and loading the libraries
+install.packages("recommenderlab", dependencies=TRUE)
+install.packages("Matrix")
+install.packages("caTools")
+install.packages("plyr")
+library(plyr)
+library(recommenderlab)
+library(Matrix)
+library(caTools)
+#movie rating data
+movie_rate_data <- read.csv('C:/Users/Sudheesh Nandakumar/Desktop/Data Science/R_for_Data_Science/Data/Movie.csv')
+#metadata about the variable
+#movie_rate_data<-Movie
+str(movie_rate_data)
+#rating distribution
+hist(movie_rate_data$rating)
+
+#the datatype should be realRatingMatrix inorder to build recommendation engine
+movie_rate_data_matrix <- as(movie_rate_data, 'realRatingMatrix')
+movie_rate_data_matrix@data
+
+#User Based Collaborative Filtering
+
+movie_recomm_model2 <- Recommender(movie_rate_data_matrix, method="UBCF")
+
+#Predictions for all users 
+recommended_items2 <- predict(movie_recomm_model2, movie_rate_data_matrix, n=5)
+d<-as(recommended_items2, "list")
+d
+
+library (plyr)
+df <- ldply (d, data.frame)
+
+write.csv(df,"RE_Movies.csv")
+
+
+
+########################
+
+
+###########################
